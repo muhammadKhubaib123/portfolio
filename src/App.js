7e169d70becdf8681acc7012c30b7104e1e7de37
Muhammad Khubaib <80357030+muhammadKhubaib123@users.noreply.github.com>
2023-11-08 10:47:39 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Khubaib Developer.
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lun React
         </a>
       </header>
     </div>
